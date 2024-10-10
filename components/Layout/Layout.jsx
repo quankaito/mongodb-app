4d7ae5b087e3f97cf3a4b7e593f526ced9954f4a
Minh Quan <nguyenminhquan170304@gmail.com>
2024-10-10 23:35:24 +0700

    fix title

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Head>
-        <title>Next.js MongoDB App</title>
+        <title>MongoDB App</title>
         <meta
           key="viewport"
           name="viewport"
